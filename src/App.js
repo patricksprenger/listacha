04b3c0f2e7eaa7acef877dcb7e52c68a9821c5a0
Patrick Sprenger Trindade <patricksprengertrindade@infracwis-MacBook-Pro.local>
2022-11-07 12:33:56 -0300

    added lint exception

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ function App() {
           console.log('decoded', decoded)
           // setCode(decoded);
     }).catch(err => console.log('err', err)); 
+// eslint-disable-next-line react-hooks/exhaustive-deps
 }, []) 
 
 useEffect(() => {
