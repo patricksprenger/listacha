acbf68a87a592ee73bb6d164a797a93a483e25f1
Patrick Sprenger Trindade <patricksprengertrindade@ip-192-168-2-107.sa-east-1.compute.internal>
2022-11-09 22:01:21 -0300

    update project

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import { Octokit } from "octokit";
 import './App.css';
 import { useCallback, useEffect, useState } from 'react';
 import { RotatingLines } from "react-loader-spinner";
+import { toast, ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.min.css'
+
+function b64DecodeUnicode(str) {
+  return decodeURIComponent(atob(str).split('').map(function(c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
+}
+
+function b64EncodeUnicode(str) {
+  return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
+      function toSolidBytes(match, p1) {
+          return String.fromCharCode('0x' + p1);
+  }));
+}
 
 function App() {
   const [state, setState] = useState(null)
   const [sha, setSha] = useState('')
   const [isLoading, setIsLoading] = useState(false)
-
-  console.log(process.env.REACT_APP_GITHUB_TOKEN, 'TOKENENENEN')
   
   const octokit = new Octokit({ 
     auth: process.env.REACT_APP_GITHUB_TOKEN,
@@ -16,21 +29,18 @@ function App() {
   
   const onLoad = useCallback(async () => {
     await octokit.request("GET /octocat", {})
-    console.log(octokit, 'métodos')
     await octokit.request('GET /repos/{owner}/{repo}/contents/list.json', {
       owner: 'patricksprenger',
       repo: 'patricksprenger.github.io',
       path: 'blob/main/list.json',
       headers: {
         'If-None-Match': ''
-      }
+      },
     }).then(res => {
           const encoded = res.data.content;
-          const decoded = JSON.parse(atob(encoded));
+          const decoded = JSON.parse(b64DecodeUnicode(encoded));
           setState(decoded)
           setSha(res.data.sha)
-          console.log('decoded', decoded)
-          // setCode(decoded);
     }).catch(err => console.log('err', err)); 
 // eslint-disable-next-line react-hooks/exhaustive-deps
 }, []) 
@@ -40,11 +50,9 @@ useEffect(() => {
 }, [onLoad])
 
 async function updateFile(updatedList) {
-  console.log(sha, 'shaaaaaaa')
   try {
-    const contentEncoded = btoa(JSON.stringify(updatedList));
+    const contentEncoded = b64EncodeUnicode(JSON.stringify(updatedList))
     const data = await octokit.rest.repos.createOrUpdateFileContents({
-    // replace the owner and email with your own details
     owner: "patricksprenger",
     repo: "patricksprenger.github.io",
     path: "list.json",
@@ -60,30 +68,46 @@ async function updateFile(updatedList) {
         email: "patricktrindade9@gmail.com",
       },
     });
-    console.log(data)
   } catch (error) {
     console.log(error)
   }
 }
 
-//  console.log(token)  // updateFile()
-
   function handleCheck(position, checked) {
     const newArray = [...state]
+    newArray[position].quantity = !checked ? newArray[position].quantity - 1 : newArray[position].quantity + 1
     newArray[position].checked = !checked
     setState(newArray)
   }
 
-  function handleUpdate() {
-    setIsLoading(true)
-    updateFile(state)
-    setTimeout(() => setIsLoading(false), 7000)
+  function updateSuccess() {
+    setIsLoading(false)
+    toast.success('Opção salva. Obrigada(o)!', {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+      });
   }
 
-  console.log(isLoading, 'loading')
+  function handleUpdate() {
+    const stateToSend = state.map((gift) => {
+      return {
+        "item": gift.item,
+        "quantity": gift.quantity,
+        "checked": gift.quantity === 0 ? true : false
+      }
+    })
+    setIsLoading(true)
+    updateFile(stateToSend)
+    setTimeout(() => updateSuccess(), 5000)
+  }
 
   function Input({ gift, index }) {
-    console.log(gift, index);
     return (
       <div className="list">
         <input type="checkbox" checked={gift.checked} className="input" onChange={() => handleCheck(index, gift.checked)}/>
@@ -95,22 +119,29 @@ async function updateFile(updatedList) {
   return (
     <>
       {isLoading ? (
-        <RotatingLines
-          strokeColor="grey"
-          strokeWidth="5"
-          animationDuration="0.75"
-          width="96"
-          visible={true}
-       />
+        <div className="loader">
+          <RotatingLines
+            strokeColor="grey"
+            strokeWidth="5"
+            animationDuration="0.75"
+            width="96"
+            visible={true}
+         />
+        </div>
       ) : (
         <>
-          <button className="btn" onClick={handleUpdate}>SALVAR</button>
+          <ToastContainer />
+          <div className="header">
+            <img className="img" src="https://images.creativemarket.com/0.1.0/ps/7424024/1820/1213/m1/fpnw/wm1/ffzblzstga22qqc5ybvuyrxpju94awstdhrlqq0wfsbuocevbesnc17upfdp6xow-.jpg?1575663214&s=bb273e1b77d3758f29d618072cb5a295" alt="background"/>
+            <p className="title">Chá de bebê</p>
+          </div>
           <div className="App-header">
             <div className='main'>
               {state?.map((item, index) => 
                 <Input gift={item} index={index} />
-              )}
+                )}
             </div>
+            <button className="btn" onClick={handleUpdate}>Salvar</button>
           </div>
         </>
       )}
